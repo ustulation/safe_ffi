bb373c90cc85a3603ae013b5b8bdb8042af78189
ustulation <ustulation@gmail.com>
2015-08-07 21:45:23 +0530

    testing complete

--- c/test/main.c
+++ c/test/main.c
@@ -23,40 +23,50 @@
 #include <stdlib.h>
 
 int main() {
-    printf("\n\nCreating path \"/a/\" ...");
+    printf("\n----------------------------------------------\n");
+    printf(  "                BEGIN TEST                    \n");
+    printf(  "----------------------------------------------\n");
+    printf("\nCreating path \"/a/\" ...\n");
     const char* absolute_path_a = "/a";
     int32_t error_code = c_create_sub_directory(absolute_path_a, true);
     assert(!error_code);
+    printf("Successful !\n");
 
-    printf("\nCreating path \"/a/last/\" ...");
+    printf("\nCreating path \"/a/last/\" ...\n");
     const char* absolute_path_a_last = "/a/last/";
     error_code = c_create_sub_directory(absolute_path_a_last, true);
     assert(!error_code);
+    printf("Successful !\n");
 
     const char* file_content = "This is the file's content.";
-    printf("\nCreating file \"/a/last/hello.txt\" with content: \"%s\" ...", file_content);
+    printf("\nCreating file \"/a/last/hello.txt\" with content: \"%s\" ...\n", file_content);
     const char* absolute_path_a_last_hello = "/a/last/hello.txt";
     error_code = c_create_file(absolute_path_a_last_hello, file_content);
     assert(!error_code);
+    printf("Successful !\n");
 
-    printf("\nGetting file size for \"/a/last/hello.txt\" ...");
+    printf("\nGetting file size for \"/a/last/hello.txt\" ...\n");
     int file_size = -1;
     int* p_file_size = &file_size;
     assert(strlen(file_content) != file_size);
     error_code = c_get_file_size(absolute_path_a_last_hello, p_file_size);
     assert(!error_code);
     assert(strlen(file_content) == file_size);
+    printf("Obtained size: %d Bytes\n", file_size);
+    printf("Successful !\n");
 
-    printf("\nGetting file content for \"/a/last/hello.txt\" ...");
+    printf("\nGetting file content for \"/a/last/hello.txt\" ...\n");
     char* content_buffer = malloc(file_size + 1);
     error_code = c_get_file_content(absolute_path_a_last_hello, content_buffer);
     assert(!error_code);
     assert(!strcmp(content_buffer, file_content));
-    printf("\nObtained data: \"%s\"", content_buffer);
+    printf("Obtained data: \"%s\"\n", content_buffer);
+    printf("Successful !\n");
 
-    printf("Freeing heap ...");
+    printf("\nFreeing heap ...\n");
     free(content_buffer);
+    printf("Successful !\n");
 
-    printf("\nExiting ...");
+    printf("\nExiting ...\n");
     return 0;
 }
